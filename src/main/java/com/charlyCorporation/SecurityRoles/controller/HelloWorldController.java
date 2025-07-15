c728139b887c94c76988903753a4fb85f8295b88
cahrly12543 <acuario12543@gmail.com>
2025-07-15 16:22:42 -0600

    Se actualiza el archivo docker-compose y se agregan perfles al msvc auth-service

--- src/main/java/com/charlyCorporation/SecurityRoles/controller/HelloWorldController.java
+++ src/main/java/com/charlyCorporation/SecurityRoles/controller/HelloWorldController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-@PreAuthorize("denyAll()")
 public class HelloWorldController {
 
 
